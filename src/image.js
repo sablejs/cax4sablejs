e8ae12bf767a72e449ec7dddafda589b64ff5f59
eroszhao <zyeros1991@gmail.com>
2021-06-21 00:46:30 +0800

    [UPD] add Image corssOrigin

--- src/image.js
+++ src/image.js
@@ -1,4 +1,19 @@
 module.exports = (vm) => {
+  if (window && window.Image) {
+    const Image = window.Image;
+    window.Image = function () {
+      if (this instanceof window.Image) {
+        const image = new Image();
+        if (vm.crossOrigin) {
+          image.crossOrigin = vm.crossOrigin;
+        }
+        return image;
+      } else {
+        throw new Error("should use new to create Image");
+      }
+    };
+  }
+
   const global = vm.getGlobal();
   const vImage = vm.createFunction("Image", function (image) {
     const vThisRef = vm.asObject(this);
